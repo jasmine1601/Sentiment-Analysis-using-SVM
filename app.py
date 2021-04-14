bd86067f4161dd3b6da0c0a584f526e43a33016d
Masuma Jasmine <57951768+jasmine1601@users.noreply.github.com>
2021-04-15 02:58:52 +0530

    1.0

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import sklearn
+import joblib
+model = joblib.load('Sentiment_Analyser')
+st.title('Sentiment Analyser')
+ip = st.text_input('Enter your review: ')
+op = model.predict([ip])
+ans=op[0]
+if st.button('Predict'):
+  if ans == 'Positive':
+      st.success("Positive :)")
+  elif ans == 'Negative':
+      st.error("Negative :(")
+  elif ans== 'Neutral':
+      st.warning("Neutral :|")
