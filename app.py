f4e64a7e59afac78ee29e87fd728d28dbc67b637
Sriniketh J <srinikethcr7@gmail.com>
2022-12-24 13:50:00 +0530

    fix: streamlit error

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import sklearn
 import joblib
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
 model = joblib.load('Sentiment_Analyser')
 st.title('Sentiment Analyser')
 ip = st.text_input('Enter your review: ')
