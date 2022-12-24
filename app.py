55471201511f34d628744d85df665a2b6aa117e1
Sriniketh J <srinikethcr7@gmail.com>
2022-12-24 14:29:25 +0530

    modify: added commented code

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import sklearn
 import joblib
-# from htbuilder import HtmlElement, div, ul, li, br, hr, a, styles, classes, fonts
-# from htbuilder.units import percent, px
-# from htbuilder.funcs import rgba, rgb
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
 model = joblib.load('Sentiment_Analyser')
 st.title('Sentiment Analyser')
@@ -17,3 +17,59 @@ if st.button('Predict'):
       st.error("Negative :(")
   elif ans== 'Neutral':
       st.warning("Neutral :|")
+
+def layout(*args):
+
+    style = """
+    <style>
+      footer {visibility: hidden;}
+     .stApp { bottom: 105px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+
+myargs = [
+        "Analyzes the sentiment of input phrases"
+    ]
+layout(*myargs)
